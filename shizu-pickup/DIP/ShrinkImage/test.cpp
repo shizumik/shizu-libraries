428bf09a701e5f457eb32b263c161d45abe251ac
Shizumi <497088711@qq.com>
2017-09-26 14:37:40 +0800

    test runtime

--- shizu-pickup/DIP/ShrinkImage/test.cpp
+++ shizu-pickup/DIP/ShrinkImage/test.cpp
@@ -1,12 +1,17 @@
 #include "skimg.h"
+#include <ctime>
 using namespace std;
-
 int main()
 {
-
+	clock_t st, end;
 	BYTE *img = LoadBMP();
+	st = clock();
+	shrinkImageX(img,2);
+	end = clock();
 	int f;
 	f = SaveBMP(img);
 	cout << f << endl;
+	cout << "runtime: " << end - st << endl;
+	system("pause");
 	return 0;
 }
